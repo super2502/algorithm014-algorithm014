57068c3039ecca889eeffd2a801240d568b65ddd
super250 <22894251@qq.com>
2020-08-10 01:32:05 +0800

    283_移动零。

--- Week_01/283_move-zeroes.go
+++ Week_01/283_move-zeroes.go
@@ -1,1 +1,17 @@
 package Week_01
+
+func moveZeroes(nums []int) {
+	l := len(nums)
+	if l <= 1 {
+		return
+	}
+	i := 0
+	j := 0
+	for j < l {
+		if nums[j] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+		}
+		j++
+	}
+}
