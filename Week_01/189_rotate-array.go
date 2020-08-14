d9576df5a136048e5360b9edb09c0a3f739b1330
super250 <22894251@qq.com>
2020-08-14 22:16:37 +0800

    189_rotate-array

--- Week_01/189_rotate-array.go
+++ Week_01/189_rotate-array.go
@@ -1,1 +1,23 @@
 package Week_01
+
+func rotate(nums []int, k int) {
+
+	l := len(nums)
+	if l == 0 {
+		return
+	}
+
+	revert(nums, 0, l-1)
+	revert(nums, 0, k%l-1)
+	revert(nums, k%l, l-1)
+
+}
+
+func revert(nums []int, i, j int) {
+	for i < j {
+		nums[i], nums[j] = nums[j], nums[i]
+		i++
+		j--
+	}
+
+}
