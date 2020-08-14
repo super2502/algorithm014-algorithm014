eef7b722154bca9d678fcc91bb0c50ae983e6988
super250 <22894251@qq.com>
2020-08-14 22:51:32 +0800

    641_design-circular-deque

--- Week_01/641_design-circular-deque.go
+++ Week_01/641_design-circular-deque.go
@@ -1,1 +1,89 @@
 package Week_01
+
+type MyCircularDeque struct {
+	arr      []int
+	head     int
+	rear     int
+	capacity int
+}
+
+/** Initialize your data structure here. Set the size of the deque to be k. */
+func Constructor(k int) MyCircularDeque {
+	return MyCircularDeque{
+		arr:      make([]int, k+1),
+		head:     0,
+		rear:     0,
+		capacity: k,
+	}
+
+}
+
+/** Adds an item at the front of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) InsertFront(value int) bool {
+	if this.IsFull() {
+		return false
+	}
+	this.head = (this.head + this.capacity) % (this.capacity + 1)
+	this.arr[this.head] = value
+
+	return true
+}
+
+/** Adds an item at the rear of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) InsertLast(value int) bool {
+	if this.IsFull() {
+		return false
+	}
+	this.arr[this.rear] = value
+	this.rear = (this.rear + 1) % (this.capacity + 1)
+	return true
+}
+
+/** Deletes an item from the front of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) DeleteFront() bool {
+	if this.IsEmpty() {
+		return false
+	}
+
+	this.head = (this.head + 1) % (this.capacity + 1)
+	return true
+}
+
+/** Deletes an item from the rear of Deque. Return true if the operation is successful. */
+func (this *MyCircularDeque) DeleteLast() bool {
+	if this.IsEmpty() {
+		return false
+	}
+	this.rear = (this.rear + this.capacity) % (this.capacity + 1)
+
+	return true
+}
+
+/** Get the front item from the deque. */
+func (this *MyCircularDeque) GetFront() int {
+	if this.IsEmpty() {
+		return -1
+	}
+
+	return this.arr[this.head]
+}
+
+/** Get the last item from the deque. */
+func (this *MyCircularDeque) GetRear() int {
+	if this.IsEmpty() {
+		return -1
+	}
+
+	rear := (this.rear + this.capacity) % (this.capacity + 1)
+	return this.arr[rear]
+}
+
+/** Checks whether the circular deque is empty or not. */
+func (this *MyCircularDeque) IsEmpty() bool {
+	return this.head == this.rear
+}
+
+/** Checks whether the circular deque is full or not. */
+func (this *MyCircularDeque) IsFull() bool {
+	return (this.rear+1)%(this.capacity+1) == this.head
+}
