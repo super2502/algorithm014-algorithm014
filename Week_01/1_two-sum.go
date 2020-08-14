bfc82215ce4b6c31d961bfcac81df7d5b4ada33a
super250 <22894251@qq.com>
2020-08-14 22:27:39 +0800

    1_two-sum

--- Week_01/1_two-sum.go
+++ Week_01/1_two-sum.go
@@ -1,1 +1,19 @@
 package Week_01
+
+func twoSum(nums []int, target int) []int {
+
+	l := len(nums)
+	if l < 2 {
+		return []int{}
+	}
+	m := make(map[int]int)
+	for i := 0; i < l; i++ {
+		idx, ok := m[target-nums[i]]
+		if ok {
+			return []int{idx, i}
+		}
+		m[nums[i]] = i
+	}
+
+	return []int{}
+}
