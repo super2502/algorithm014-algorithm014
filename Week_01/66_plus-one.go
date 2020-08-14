8a0eda5aa8a8903525fce4fe9a06a237955c96f0
super250 <22894251@qq.com>
2020-08-14 22:34:27 +0800

    66_plus-one

--- Week_01/66_plus-one.go
+++ Week_01/66_plus-one.go
@@ -1,1 +1,26 @@
 package Week_01
+
+func plusOne(digits []int) []int {
+
+	l := len(digits)
+	if l == 0 {
+		return []int{}
+	}
+	carry := 0
+	for i := l - 1; i >= 0; i-- {
+		var sum int
+		if i == l-1 {
+			sum = digits[i] + 1
+		} else {
+			sum = digits[i] + carry
+		}
+		digits[i] = sum % 10
+		carry = sum / 10
+	}
+
+	if carry == 0 {
+		return digits
+	}
+	return append([]int{1}, digits...)
+
+}
