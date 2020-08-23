bd3e43221b5b857d938993977f2c0d8eb75019bb
super250 <22894251@qq.com>
2020-08-23 15:22:46 +0800

    17_letter-combinations-of-a-phone-number

---
+++ Week_03/17_letter-combinations-of-a-phone-number.go
@@ -0,0 +1,47 @@
+package Week_03
+
+var sret []string
+var numChars map[int][]byte
+
+func letterCombinations(digits string) []string {
+	l := len(digits)
+	if l == 0 {
+		return []string{}
+	}
+	nums := make([]int, l)
+	for i := 0; i < l; i++ {
+		nums[i] = int(digits[i] - '0')
+	}
+	sret = make([]string, 0)
+	numChars = map[int][]byte{
+		2: {'a', 'b', 'c'},
+		3: {'d', 'e', 'f'},
+		4: {'g', 'h', 'i'},
+		5: {'j', 'k', 'l'},
+		6: {'m', 'n', 'o'},
+		7: {'p', 'q', 'r', 's'},
+		8: {'t', 'u', 'v'},
+		9: {'w', 'x', 'y', 'z'},
+	}
+	dfs17([]int{}, nums, []byte{})
+	return sret
+}
+
+func dfs17(path []int, nums []int, ret []byte) {
+	//fmt.Printf("called with path(%+v), ret(%+v)\n", path, ret)
+	if len(path) == len(nums) {
+		sret = append(sret, string(ret))
+		//fmt.Printf("returned (%+v)\n", string(ret))
+		return
+	}
+	num := nums[len(path)]
+	bs := numChars[num]
+	path = append(path, num)
+	for _, b := range bs {
+		retCopy := make([]byte, len(ret))
+		copy(retCopy, ret)
+		retCopy = append(retCopy, b)
+		//fmt.Printf("call next %+v (%+v)\n", path, retCopy)
+		dfs17(path, nums, retCopy)
+	}
+}
---
+++ Week_03/17_letter-combinations-of-a-phone-number_test.go
@@ -0,0 +1,9 @@
+package Week_03
+
+import "testing"
+
+func Test_dfs17(t *testing.T) {
+	s := letterCombinations("23")
+	t.Logf("s: %+v", s)
+
+}
