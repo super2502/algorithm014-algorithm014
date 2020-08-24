e3246592462aa3457b4af35654e0e313909ac0e4
super250 <22894251@qq.com>
2020-08-25 00:37:17 +0800

    236_lowest-common-ancestor-of-a-binary-tree

---
+++ Week_03/236_lowest-common-ancestor-of-a-binary-tree.go
@@ -0,0 +1,27 @@
+package Week_03
+
+//* Definition for TreeNode.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root == p || root == q {
+		return root
+	}
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	} else if left != nil {
+		return left
+	} else {
+		return right
+	}
+}
