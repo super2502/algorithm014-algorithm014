cc992e75da354996608553361324c5d2214a9428
super250 <22894251@qq.com>
2020-08-29 17:07:40 +0800

    40_combination-sum-ii

---
+++ Week_03/40_combination-sum-ii.go
@@ -0,0 +1,33 @@
+package Week_03
+
+import "sort"
+
+var ret40 [][]int
+
+func combinationSum2(candidates []int, target int) [][]int {
+	ret40 = make([][]int, 0)
+	sort.Ints(candidates)
+	dfs40([]int{}, 0, candidates, target)
+	return ret40
+}
+
+func dfs40(path []int, start int, candidates []int, target int) {
+	if target < 0 {
+		return
+	}
+	if target == 0 {
+		p := make([]int, len(path))
+		copy(p, path)
+		ret40 = append(ret40, p)
+		return
+	}
+	for i := start; i < len(candidates); i++ {
+		if i > start && candidates[i] == candidates[i-1] {
+			continue
+		}
+		path = append(path, candidates[i])
+		dfs40(path, i+1, candidates, target-candidates[i])
+		path = path[:len(path)-1]
+	}
+
+}
