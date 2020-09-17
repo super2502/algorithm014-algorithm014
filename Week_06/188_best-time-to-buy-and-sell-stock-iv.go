c7c2e4e2839c47de75ba469a591b759ac57ab63a
super250 <22894251@qq.com>
2020-09-18 00:11:26 +0800

    188_best-time-to-buy-and-sell-stock-iv

--- Week_06/188_best-time-to-buy-and-sell-stock-iv.go
+++ Week_06/188_best-time-to-buy-and-sell-stock-iv.go
@@ -2,32 +2,37 @@ package Week_06
 
 func maxProfit(k int, prices []int) int {
 	n := len(prices)
-	if n == 0 {
+	if n <= 1 {
 		return 0
 	}
-	ldp := make([][]int, 2)
-	ldp[0] = make([]int, n)
-	ldp[1] = make([]int, n)
-	ldp[0][0] = 0
-	ldp[1][0] = -1 * prices[0]
-
-	maxP := 0
-	for j := 1; j <= k; j++ {
-		cdp := make([][]int, 2)
-		cdp[0] = make([]int, n)
-		cdp[1] = make([]int, n)
-		cdp[0][0] = 0
-		cdp[1][0] = -1 * prices[0]
-
+	if k > n/2 {
+		sum := 0
 		for i := 1; i < n; i++ {
-			cdp[0][i] = max188(cdp[0][i-1], ldp[1][i-1]+prices[i])
-			cdp[1][i] = max188(cdp[1][i-1], cdp[0][i-1]-prices[i])
+			if prices[i] > prices[i-1] {
+				sum += prices[i] - prices[i-1]
+			}
 		}
-		maxP = max188(maxP, cdp[0][n-1])
-		//ldp = cdp
-		deepCopy(cdp, ldp)
+		return sum
 	}
-	return maxP
+	dp := make([][][]int, k+1)
+	for j := 0; j <= k; j++ {
+		dp[j] = make([][]int, 2)
+		dp[j][0] = make([]int, n)
+		dp[j][1] = make([]int, n)
+		dp[j][1][0] = -1 * prices[0]
+	}
+
+	maxRet := 0
+	for j := 0; j <= k; j++ {
+		for i := 1; i < n; i++ {
+			if j > 0 {
+				dp[j][0][i] = max188(dp[j][0][i-1], dp[j-1][1][i-1]+prices[i])
+			}
+			dp[j][1][i] = max188(dp[j][1][i-1], dp[j][0][i-1]-prices[i])
+		}
+		maxRet = max188(maxRet, dp[j][0][n-1])
+	}
+	return maxRet
 }
 
 func max188(a, b int) int {
@@ -35,12 +40,4 @@ func max188(a, b int) int {
 		return a
 	}
 	return b
-}
-
-func deepCopy(src, dest [][]int) {
-	for i := 0; i < len(src); i++ {
-		for j := 0; j < len(src[0]); j++ {
-			dest[i][j] = src[i][j]
-		}
-	}
 }
