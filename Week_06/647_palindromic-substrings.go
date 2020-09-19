90abe878cbdb07516273aed9c77cd51c25bf1201
madongdong8 <madongdong1@jd.com>
2020-09-19 14:22:22 +0800

    647_palindromic-substrings

--- Week_06/647_palindromic-substrings.go
+++ Week_06/647_palindromic-substrings.go
@@ -2,6 +2,30 @@ package Week_06
 
 func countSubstrings(s string) int {
 
+	count := 0
+	for i := 0; i<len(s);i++ {
+		count += is(s, i, i)
+		if i != len(s) - 1 {
+			count += is(s, i, i+1)
+		}
+	}
+	return count
+}
+
+func is(s string, i, j int) int {
+	count := 0
+	for i >= 0 && j < len(s) {
+		if i == j  {
+			count++
+		} else if s[i] == s[j] {
+			count++
+		} else {
+			break
+		}
+		i--
+		j++
+	}
+	return count
 }
 
 
