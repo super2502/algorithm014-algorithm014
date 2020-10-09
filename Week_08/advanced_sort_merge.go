f69dd0a4d7c05c29e51e257fa404071579538020
madongdong8 <madongdong1@jd.com>
2020-10-09 17:25:11 +0800

    quick merge & merge sort iteration mode

--- Week_08/advanced_sort_merge.go
+++ Week_08/advanced_sort_merge.go
@@ -42,3 +42,55 @@ func merge(nums []int, left, mid, right int) {
 	}
 	copy(nums[left:right+1], tmp)
 }
+
+func mergeSortIteration(nums []int) {
+	n := len(nums)
+	step := 1
+	for {
+		//fmt.Printf("step: %v, before merge, %+v\n", step, nums)
+		for i := 0; i < n ; i+=step<<1 {
+			mergeIteration(nums, i, step)
+		}
+		//fmt.Printf("step: %v, after merge, %+v\n", step, nums)
+		step = step << 1
+		if step >= n {
+			break
+		}
+	}
+
+}
+
+func mergeIteration(nums []int, start int, step int) {
+	//fmt.Printf("step: %v, before merge, %+v\n", step, nums)
+	//defer func() {
+	//	fmt.Printf("step: %v, after merge, %+v\n", step, nums)
+	//}()
+	tmp := make([]int, 0, step * 2)
+	if start + step >= len(nums) {
+		return
+	}
+
+	end := start + 2 * step
+	if end >= len(nums) {
+		end = len(nums)
+	}
+	i, j := start, start + step
+	for i < start + step && j < end {
+		if nums[i] <= nums[j] {
+			tmp = append(tmp, nums[i])
+			i++
+		} else {
+			tmp = append(tmp, nums[j])
+			j++
+		}
+	}
+	for i < start + step {
+		tmp = append(tmp, nums[i])
+		i++
+	}
+	for j < end {
+		tmp = append(tmp, nums[j])
+		j++
+	}
+	copy(nums[start: end], tmp)
+}
--- Week_08/advanced_sort_quick.go
+++ Week_08/advanced_sort_quick.go
@@ -29,3 +29,44 @@ func partition(nums []int, i, j int) int{
 	nums[pivot], nums[start] = nums[start], nums[pivot]
 	return start
 }
+
+func quickSortIteration(nums []int) {
+
+	s := make(stack, 0)
+	s.Push([]int{0, len(nums)-1})
+	for !s.IsEmpty() {
+		x := s.Pop().([]int)
+		start, end := x[0], x[1]
+		if start >= end {
+			continue
+		}
+		pivot := end
+		idx := start
+		for i := start; i < end; i++ {
+			if nums[i] < nums[pivot] {
+				nums[idx], nums[i] = nums[i], nums[idx]
+				idx++
+			}
+		}
+		nums[pivot], nums[idx] = nums[idx], nums[pivot]
+		s.Push([]int{start, idx - 1})
+		s.Push([]int{idx + 1, end})
+	}
+}
+
+type stack []interface{}
+
+func (s *stack) Len() int {
+	return len(*s)
+}
+func (s *stack) IsEmpty() bool {
+	return s.Len() == 0
+}
+func (s *stack) Push(x interface{}) {
+	*s = append(*s, x)
+}
+func (s *stack) Pop() interface{} {
+	x := (*s)[s.Len() - 1]
+	*s = (*s)[:s.Len() - 1]
+	return x
+}
