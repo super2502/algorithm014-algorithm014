7b46eb244d251779d959b82c14587f057412d7b5
madongdong8 <madongdong1@jd.com>
2020-10-10 11:07:18 +0800

    update search

--- Week_08/searchFirst.go
+++ Week_08/searchFirst.go
@@ -1,6 +1,6 @@
-package Week_08
+package main
 
-func searchFirstLessEq(nums []int, target int) int {
+func searchLastLessEq(nums []int, target int) int {
 	l, r := 0, len(nums) - 1
 	for l <= r {
 		mid := l + ( r - l ) >> 1
@@ -18,6 +18,44 @@ func searchFirstLargeEq(nums []int, target int) int {
 	for l <= r {
 		mid := l + ( r - l ) >> 1
 		if nums[mid] >= target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return l
+}
+
+func searchFirstEq(nums []int, target int) int {
+	l, r := 0, len(nums) - 1
+	for l <= r {
+		mid := l + ( r - l ) >> 1
+		if nums[mid] == target {
+			if mid > 0 && nums[mid] == nums[mid-1] {
+				r = mid - 1
+			} else {
+				return mid
+			}
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return -1
+}
+
+func searchLastEq(nums []int, target int) int {
+	l, r := 0, len(nums) - 1
+	for l <= r {
+		mid := l + ( r - l ) >> 1
+		if nums[mid] == target {
+			if mid < len(nums) - 1 && nums[mid] == nums[mid+1] {
+				l = mid + 1
+			} else {
+				return mid
+			}
+		} else if nums[mid] > target {
 			r = mid - 1
 		} else {
 			l = mid + 1
---
+++ Week_08/search_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFirstLessEq(t *testing.T) {
+
+	nums := []int{1,4,6,6,7}
+	t.Logf("%v", nums)
+	t.Logf("============= testng last less eq")
+	for _, target := range []int{
+		0,1,2,3,4,5,6,7,8,
+	} {
+		var i int
+		i = searchLastLessEq(nums, target)
+		t.Logf("target: %v, idx: %v", target, i)
+	}
+	t.Logf("============= testng first large eq")
+	for _, target := range []int{
+		0,1,2,3,4,5,6,7,8,
+	} {
+		var i int
+		i = searchFirstLargeEq(nums, target)
+		t.Logf("target: %v, idx: %v", target, i)
+	}
+
+}
+
+func TestFirstLastEq(t *testing.T) {
+	nums := []int{1,4,6,6,6,7}
+	nums = []int{0,1,2,3,4,5,6,6,6,7}
+	t.Logf("%v", nums)
+	t.Logf("============= testng first eq")
+	for _, target := range []int{
+		6,
+	} {
+		var i int
+		i = searchFirstEq(nums, target)
+		t.Logf("target: %v, idx: %v", target, i)
+	}
+
+	t.Logf("============= testng last eq")
+	for _, target := range []int{
+		6,
+	} {
+		var i int
+		i = searchLastEq(nums, target)
+		t.Logf("target: %v, idx: %v", target, i)
+	}
+
+}
+
