f565af9be2abc2b13442d9064459020a7405e67b
super250 <22894251@qq.com>
2020-10-12 22:38:16 +0800

    83_remove-duplicates-from-sorted-list

---
+++ Week_09/Day64/83_remove-duplicates-from-sorted-list.go
@@ -0,0 +1,18 @@
+package Day64
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+
+	cur := head
+	for cur != nil {
+		for cur.Next != nil && cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		}
+		cur = cur.Next
+	}
+	return head
+}
