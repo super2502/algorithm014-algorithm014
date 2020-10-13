443cdf69d33e1c940c7a0feb446bd3c94e0a18fb
super250 <22894251@qq.com>
2020-10-13 22:33:28 +0800

    44_wildcard-matching

--- Week_09/44_wildcard-matching.go
+++ Week_09/44_wildcard-matching.go
@@ -2,5 +2,27 @@ package Week_09
 
 func isMatch(s string, p string) bool {
 
-	return false
+	m := len(p)
+	n := len(s)
+	dp := make([][]bool, m+1)
+	for i := 0; i <= m; i++ {
+		dp[i] = make([]bool, n+1)
+	}
+	dp[0][0] = true
+	for i := 1; i <= m; i++ {
+		if p[i-1] == '*' {
+			dp[i][0] = dp[i-1][0]
+		}
+		for j := 1; j <= n; j++ {
+			if p[i-1] == '*' {
+				dp[i][j] = dp[i-1][j] || dp[i-1][j-1] || dp[i][j-1]
+			} else {
+				if p[i-1] == '?' || p[i-1] == s[j-1] {
+					dp[i][j] = dp[i-1][j-1]
+				}
+			}
+
+		}
+	}
+	return dp[m][n]
 }
