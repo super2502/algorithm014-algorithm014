76e31127dc70410884d52e6a6f8da38443f425be
super250 <22894251@qq.com>
2020-10-15 00:12:58 +0800

    818_race-car

---
+++ Week_09/818_race-car.go
@@ -0,0 +1,6 @@
+package Week_09
+
+func racecar(target int) int {
+
+	return 0
+}
---
+++ Week_09/818_race-car_test.go
@@ -0,0 +1,13 @@
+package Week_09
+
+import "testing"
+
+func Test_racecar(t *testing.T) {
+	for _, ca := range [][]int{
+		{3, 2},
+		{6, 5},
+	} {
+		ret := racecar(ca[0])
+		t.Logf("target: %v, expect: %v, yours: %v", ca[0], ca[1], ret)
+	}
+}
