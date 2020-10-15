22a9ba29d0fb1e13fa47670cbbc18d7f2f0f280b
super250 <22894251@qq.com>
2020-10-16 00:43:30 +0800

    818_race-car

--- Week_09/818_race-car.go
+++ Week_09/818_race-car.go
@@ -1,32 +1,27 @@
 package Week_09
 
-import (
-	"fmt"
-)
+import "math"
 
 func racecar(target int) int {
 
-	meters := make([]int, target + 1)
-	speed := 1
-	wp := 0
-	nextWp := 0
-	for i := 0; i <= target; i++ {
-		if i == nextWp {
-			wp = nextWp
-			nextWp = wp + speed
-			speed *= 2
-			if nextWp <= target {
-				meters[nextWp] = meters[wp] + 1
+	dp := make([]int, target+1)
+	for i := 1; i <= target; i++ {
+		dp[i] = math.MaxInt64
+		for forward := 1; (1<<forward)-1 < 2*i; forward++ {
+			j := (1 << forward) - 1
+			if j == i {
+				dp[i] = forward
+			} else if j > i {
+				dp[i] = min(dp[i], forward+1+dp[j-i])
+			} else {
+				for back := 0; back < forward; back++ {
+					k := (1 << back) - 1
+					dp[i] = min(dp[i], forward+1+back+1+dp[i-j+k])
+				}
 			}
-		} else {
-			fromLeft := meters[wp] + 2 + meters[i-wp]
-			fromRight := meters[wp] + 1 + 1 + meters[nextWp - i]
-			meters[i] = min(fromLeft, fromRight)
 		}
 	}
-	fmt.Printf("%+v, %v\n", meters, nextWp)
-
-	return meters[target]
+	return dp[target]
 }
 
 func min(a, b int) int {
--- Week_09/818_race-car_test.go
+++ Week_09/818_race-car_test.go
@@ -6,6 +6,7 @@ func Test_racecar(t *testing.T) {
 	for _, ca := range [][]int{
 		{3, 2},
 		{6, 5},
+		{5, 7},
 	} {
 		ret := racecar(ca[0])
 		t.Logf("target: %v, expect: %v, yours: %v", ca[0], ca[1], ret)
