54d7d9df8fa5d80d5f9e9b5ec06efa3cf19d016d
madongdong8 <madongdong1@jd.com>
2020-10-15 11:50:26 +0800

    818_race-car

--- Week_09/818_race-car.go
+++ Week_09/818_race-car.go
@@ -1,6 +1,37 @@
 package Week_09
 
+import (
+	"fmt"
+)
+
 func racecar(target int) int {
 
-	return 0
+	meters := make([]int, target + 1)
+	speed := 1
+	wp := 0
+	nextWp := 0
+	for i := 0; i <= target; i++ {
+		if i == nextWp {
+			wp = nextWp
+			nextWp = wp + speed
+			speed *= 2
+			if nextWp <= target {
+				meters[nextWp] = meters[wp] + 1
+			}
+		} else {
+			fromLeft := meters[wp] + 1 + meters[i-wp]
+			fromRight := meters[wp] + 1 + 2 + meters[nextWp - i]
+			meters[i] = min(fromLeft, fromRight)
+		}
+	}
+	fmt.Printf("%+v, %v\n", meters, nextWp)
+
+	return meters[target]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
