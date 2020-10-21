6b6403c5fba27b9b97677aa0e46e24a036f26445
madongdong8 <madongdong1@jd.com>
2020-10-21 21:06:39 +0800

    drop_egg

---
+++ Week_10/drop_egg_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestDropEgg(t *testing.T) {
+	level := 100
+	cnt := 2
+	ret := dropEggs(level, cnt)
+
+
+	t.Logf("level %v, eggs %v, expect %v, yours %v", level, cnt, 14,ret)
+}
