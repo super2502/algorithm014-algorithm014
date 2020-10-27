c16a32f92cc711301f62bee9434c561096e9aa71
super250 <22894251@qq.com>
2020-10-27 22:04:00 +0800

    112_path-sum

---
+++ Week_11/Day79_107/112_path-sum.go
@@ -0,0 +1,14 @@
+package Day79_107
+
+func hasPathSum(root *TreeNode, sum int) bool {
+	if root == nil {
+		return false
+	}
+	if root.Left == nil && root.Right == nil {
+		if root.Val == sum {
+			return true
+		}
+		return false
+	}
+	return hasPathSum(root.Left, sum-root.Val) || hasPathSum(root.Right, sum-root.Val)
+}
