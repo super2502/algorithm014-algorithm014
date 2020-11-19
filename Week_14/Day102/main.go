855f193293eaf3eb4148a2a0680bbeba7867f5ff
madongdong8 <madongdong1@jd.com>
2020-11-19 20:56:04 +0800

    pc

---
+++ Week_14/Day102/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	//"fmt"
+)
+func maxArea(nums []int) int {
+	if len(nums) == 0 { return 0 }
+	s := make(stack, 0)
+	area := 0
+	s.Push(-1)
+	for i := 0; i < len(nums); i++ {
+		for !s.IsEmpty() && nums[i] <= nums[s.Peek()] {
+			idx := s.Pop()
+			area = max(area, nums[idx] * (i - s.Peek() - 1))
+		}
+		s.Push(i)
+	}
+	for !s.IsEmpty() {
+		idx := s.Pop()
+		area = max(area, nums[idx] * (len(nums) - s.Peek() - 1))
+	}
+	return area
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxWa(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	s := make(stack, 0)
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		for !s.IsEmpty() && nums[i] > nums[s.Peek()] {
+			idx := s.Pop()
+			if !s.IsEmpty() {
+				sum += (min(nums[i], nums[s.Peek()]) - nums[idx]) * (i - s.Peek() - 1)
+			}
+		}
+		s.Push(i)
+	}
+
+	return sum
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+type stack []int
+func (s *stack) Len() int {
+	return len(*s)
+}
+func (s *stack) IsEmpty() bool {
+	return s.Len() == 0 || (*s)[s.Len()-1] == -1
+}
+func (s *stack) Push(x int) {
+	*s = append(*s, x)
+}
+func (s *stack) Pop() int {
+	x := (*s)[s.Len() - 1]
+	*s = (*s)[:s.Len() - 1]
+	return x
+}
+func (s *stack) Peek() int {
+	x := (*s)[s.Len() - 1]
+	return x
+}
---
+++ Week_14/Day102/perfectRectangle.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"math"
+	"fmt"
+)
+
+func perfect(rectangles [][]int) bool {
+
+	minX, minY := math.MaxInt64, math.MaxInt64
+	maxX, maxY := math.MinInt64, math.MinInt64
+
+	for _, rec := range rectangles {
+		minX = min(minX, rec[0])
+		minX = min(minX, rec[2])
+		minY = min(minY, rec[1])
+		minY = min(minY, rec[3])
+		maxX = max(maxX, rec[0])
+		maxX = max(maxX, rec[2])
+		maxY = max(maxY, rec[1])
+		maxY = max(maxY, rec[3])
+	}
+
+	fmt.Printf("(%v)(%v)(%v)(%v)\n", minX, minY, maxX, maxY)
+
+	area := make([][]int, maxX )
+	for i:=0; i<len(area);i++{
+		area[i] = make([]int, maxY )
+	}
+
+	for _, rec := range rectangles {
+		x0, x1 := rec[0], rec[2]
+		if x0 > x1 {
+			x0, x1 = x1, x0
+		}
+		y0, y1 := rec[1], rec[3]
+		if y0 > y1 {
+			y0, y1 = y1, y0
+		}
+		for x := x0; x < x1 ; x++ {
+			for y := y0; y < y1; y++ {
+				//fmt.Printf("x (%v)(%v-%v), y(%v)(%v-%v)\n", x, x0, x1,y,  y0, y1)
+				area[x][y]++
+			}
+		}
+
+	}
+	for i:=minX; i<len(area);i++{
+		for j:=minY; j<len(area[0]); j++ {
+			if area[i][j] != 1 {
+				return false
+			}
+		}
+	}
+	return true
+
+}
