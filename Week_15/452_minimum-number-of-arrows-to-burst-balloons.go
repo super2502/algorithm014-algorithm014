52da3d69b2bcf40bed9be66a22601feeed44fa1e
super250 <22894251@qq.com>
2020-11-23 22:25:47 +0800

    452_minimum-number-of-arrows-to-burst-balloons

---
+++ Week_15/452_minimum-number-of-arrows-to-burst-balloons.go
@@ -0,0 +1,40 @@
+package Week_15
+
+import "sort"
+
+func findMinArrowShots(points [][]int) int {
+	n := len(points)
+	if n <= 1 {
+		return n
+	}
+	sort.Slice(points, func(i, j int) bool {
+		if points[i][0] < points[j][0] {
+			return true
+		} else if points[i][0] > points[j][0] {
+			return false
+		} else {
+			return points[i][1] < points[j][1]
+		}
+	})
+	cnt := 1
+	_, end := points[0][0], points[0][1]
+	for i := 1; i < n; i++ {
+		if points[i][0] > end {
+			cnt++
+			//start = points[i][0]
+			end = points[i][1]
+		} else {
+			//start = points[i][0]
+			end = min(end, points[i][1])
+		}
+	}
+	return cnt
+
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ Week_15/452_minimum-number-of-arrows-to-burst-balloons_test.go
@@ -0,0 +1,25 @@
+package Week_15
+
+import "testing"
+
+func Test_findMinArrowShots(t *testing.T) {
+	type args struct {
+		points [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"t1", args{[][]int{{10, 16}, {2, 8}, {1, 6}, {7, 12}}}, 2},
+		{"t2", args{[][]int{{1, 2}, {3, 4}, {5, 6}, {7, 8}}}, 4},
+		{"t3", args{[][]int{{1, 2}, {2, 3}, {3, 4}, {4, 5}}}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMinArrowShots(tt.args.points); got != tt.want {
+				t.Errorf("findMinArrowShots() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
