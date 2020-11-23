db558ac535fca4e3712350e2f09a3d1c38621645
super250 <22894251@qq.com>
2020-11-23 22:45:26 +0800

    714_best-time-to-buy-and-sell-stock-with-transaction-fee

---
+++ Week_15/714_best-time-to-buy-and-sell-stock-with-transaction-fee.go
@@ -0,0 +1,23 @@
+package Week_15
+
+func maxProfit(prices []int, fee int) int {
+	n := len(prices)
+	if n <= 1 {
+		return 0
+	}
+	dp0 := 0
+	dp1 := -prices[0]
+	for i := 1; i < n; i++ {
+		dp0Tmp := dp0
+		dp0 = max(dp0, dp1+prices[i]-fee)
+		dp1 = max(dp1, dp0Tmp-prices[i])
+	}
+	return dp0
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
